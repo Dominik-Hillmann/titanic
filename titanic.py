2209703f4a08467892ed307be4167eb7ffb07204
Dominik-Hillmann <dominik.hillmann@gmx.de>
2018-08-13 21:26:47 +0200

    Add files via upload

--- titanic.py
+++ titanic.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plot
+import scipy.stats as stat
 
 attributeNames = [
 	'survival',	# 0 = No, 1 = Yes, to be predicted
@@ -17,16 +18,50 @@ attributeNames = [
 
 trainData = pd.read_csv(
 	'./data/train.csv', 
-	na_values = ['NaN'],
+	na_values = ['NaN']
 )
 
 # print(trainData.describe())
-print(trainData.head(15))
+# print(trainData.head(15))
 trainData = trainData.drop(['Name', 'Ticket', 'Cabin'], axis = 1)
 trainData.replace(('male', 'female'), (1, 0), inplace = True)
 trainData.replace(('C', 'Q', 'S'), (0, 1, 2), inplace = True)
-print(trainData.head(15))
+# print(trainData.head(15))
 
-cols = trainData.columns
-print(cols)
-cols = [x if x % 2 else None for x in items]
+# print(trainData.columns)
+trainData.columns = [attr if attr != 'Sex' else 'Male' for attr in trainData.columns]
+trainData = trainData.dropna()
+# print(trainData.describe())
+
+trainY = trainData['Survived'].values
+trainX = trainData.drop(['Survived'], axis = 1)#.values
+attrX = trainX.columns # for working with columnsindex = np.where(arr == 'colName')
+trainX = trainX.values
+
+# print(trainY.shape, trainX.shape, len(attrX) == len(trainX[0]))
+
+# let's take a look whether individual attributes correlate with a passenger's survival
+print(trainY)
+print(trainX[:, np.where(attrX == 'Male')].flatten())
+
+for attr in attrX:
+	dta = trainX[:, np.where(attrX == attr)].flatten()
+	print(
+		'Correlation of \"Survived\" and \"{}\": {}'.format( 
+			attr,
+			np.corrcoef(trainY, dta)[0, 1]
+		)
+	)
+
+	slope, intercept, rSqu, p, stdErr = stat.linregress(trainY, dta)
+	def f(x):
+		return intercept + slope * x
+
+
+	plot.scatter(trainY, dta)
+	plot.xlabel('Survived')
+	plot.ylabel(attr)
+	plot.plot(dta, f(dta), c = 'r')
+	plot.xlim([0.0, 1.0])
+	plot.show()
+
