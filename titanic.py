7849398d64182d79cb126f7a6eff57bc1f38eff5
Dominik-Hillmann <dominik.hillmann@gmx.de>
2018-08-16 22:11:43 +0200

    Add files via upload

--- titanic.py
+++ titanic.py
@@ -3,57 +3,44 @@ import numpy as np
 import matplotlib.pyplot as plot
 import scipy.stats as stat
 
-attributeNames = [
-	'survival',	# 0 = No, 1 = Yes, to be predicted
-	'pclass',	# ticket class; 1 = 1st, 2 = 2nd, 3 = 3rd
-	'sex',
-	'age',
-	'sibsp',	#  number of siblings / spouses aboard
-	'parch',	# number of parents / children aboard
-	'ticket',	# ticket number
-	'fare',		# price the passenger was charged with
-	'cabin',	# Cabin number
-	'embarked' 	# Port of Embarkation: C = Cherbourg, Q = Queenstown, S = Southampton
-]
-
 trainData = pd.read_csv(
 	'./data/train.csv', 
-	na_values = ['NaN']
+	na_values = ['']
+)
+testData = pd.read_csv(
+	'./data/test.csv',
+	na_values = ['']
 )
 
-# print(trainData.describe())
-# print(trainData.head(15))
-trainData = trainData.drop(['Name', 'Ticket', 'Cabin'], axis = 1)
+trainData = trainData.drop(['Name', 'Ticket', 'Cabin', 'PassengerId'], axis = 1)
+# I drop these attributes since I think that either:
+#	- they don't contain information useful to know whether someone survived (Name, Passenger ID)
+#	- I don't have the means to dissern useful information from it (all three, Cabin and Ticket because maybe they contain information about where cabin was within the ship
+
 trainData.replace(('male', 'female'), (1, 0), inplace = True)
-trainData.replace(('C', 'Q', 'S'), (0, 1, 2), inplace = True)
-# print(trainData.head(15))
-
-# print(trainData.columns)
 trainData.columns = [attr if attr != 'Sex' else 'Male' for attr in trainData.columns]
-trainData = trainData.dropna()
-# print(trainData.describe())
 
-trainY = trainData['Survived'].values
-trainX = trainData.drop(['Survived'], axis = 1)#.values
-attrX = trainX.columns # for working with columnsindex = np.where(arr == 'colName')
-trainX = trainX.values
+trainData.dropna(inplace = True)
+trainData.reset_index(drop = True, inplace = True)
 
-# print(trainY.shape, trainX.shape, len(attrX) == len(trainX[0]))
+trainY = trainData['Survived']
+trainX = trainData.drop(['Survived'], axis = 1)
 
 # let's take a look whether individual attributes correlate with a passenger's survival
 # print(trainY)
 # print(trainX[:, np.where(attrX == 'Male')].flatten())
 
-for attr in attrX:
-	col = trainX[:, np.where(attrX == attr)].flatten()
-	print(
-		'Correlation of \"Survived\" and \"{}\": {}'.format( 
+for attr in trainX.columns:
+	xCol = trainX[attr].values.flatten()
+	try:
+		print('Correlation of \"Survived\" and \"{}\": {}'.format( 
 			attr,
-			np.corrcoef(trainY, col)[0, 1]
-		)
-	)
+			np.corrcoef(trainY, xCol)[0, 1]
+		))
+	except Exception: 
+		continue # embarked does not yet contain any numbers and will throw an error
 
-	slope, intercept, rSqu, p, stdErr = stat.linregress(trainY, col)
+	# slope, intercept, rSqu, p, stdErr = stat.linregress(trainY, col)
 
 	# plot.scatter(trainY, col)
 	# plot.xlabel('Survived')
@@ -62,37 +49,62 @@ for attr in attrX:
 	# plot.xlim([0.0, 1.0])
 	# plot.show()
 
-# change ('C', 'Q', 'S') == (0, 1, 2) to hot-one encoding
-print(trainData.head())
-embarked = trainData[['Embarked']].values.flatten()
+def colToHotOne(X, colName, newNameArr):
+
+# change ('C', 'Q', 'S') to hot-one encoding
+embarked = trainX['Embarked'].values.flatten()
 print(len(embarked))
 
-
 embarkedOneHot = pd.DataFrame(
-	np.array([np.array([1, 0, 0]) if port == 0 else (np.array([0, 1, 0]) if port == 1 else np.array([0, 0, 1])) for port in embarked]),
-	columns = ['Cherbourg', 'Queenstown', 'Southampton']
+	np.array([np.array([1, 0, 0] if port == 'C' else ([0, 1, 0] if port == 'Q' else [0, 0, 1])) for port in embarked]),
+	columns = ['C', 'Q', 'S']
 )
 
-print(embarkedOneHot.describe())
-print(trainData.describe())
-print(len(trainData[['Male']].values), len(embarkedOneHot.values))
-trainOld = trainData
-trainData = trainData + embarkedOneHot
-for attr in trainData.columns:
-	print(len(trainData[[attr]]))
+trainX = trainX.assign(
+	C = embarkedOneHot['C'].values,
+	S = embarkedOneHot['S'].values,
+	Q = embarkedOneHot['Q'].values
+)
+trainX.drop(['Embarked'], inplace = True, axis = 1)
 
-for attr in embarkedOneHot.columns:
-	trainData[[attr]] = embarkedOneHot[[attr]]
-for attr in trainOld.columns:
-	trainData[[attr]] = trainOld[[attr]]
-# trainData = trainData.dropna()
+# now we will change the class (1, 2, 3) to one hot encoding, too
+pClass = trainX['Pclass'].values.flatten()
+embarkedOneHot = pd.DataFrame(
+	np.array([np.array([1, 0, 0] if passenger == 1 else ([0, 1, 0] if passenger == 2 else [0, 0, 1])) for passenger in pClass]),
+	columns = ['class1', 'class2', 'class3']
+)
+trainX = trainX.assign(
+	class1 = embarkedOneHot['class1'].values,
+	class2 = embarkedOneHot['class2'].values,
+	class3 = embarkedOneHot['class3'].values
+)
+trainX.drop(['Pclass'], inplace = True, axis = 1)
+print(trainX.head(20))
 
-for attr in trainData.columns:
-	print(len(trainData[[attr]]))
-# trainData = trainData.add(embarkedOneHot[['Queenstown']], axis = 'columns')
-# print(trainData.head(40))
-# print(trainOld.head(40))
-print(len(trainData[['Male']]), len(trainData[['Queenstown']]))
-# trainData = pd.concat(trainData, embarkedOneHot, axis = 1)
-# print(trainData.head(30))
-# trainData = trainData.drop(['Embarked'])
+
+# print(trainX.head(50))
+# # Problem ist hier gefunden:
+# # 	embarkedOneHot hat fortlaufende Indizes von 1 bis 712,
+# # 	waehrend trainData fehlende Indizes besitzt, weil Punkte durch .dropna() geloescht worden sind
+# # 	entweder muss embarkedOneHot wie trainData indiziert werden oder vice versa
+# # 	vermutlich leichter durch Methode trainData neu fortlaufend zu indexieren
+# # 	danach sollte obige Operation ohne Entstehung von NaNs funktionieren.
+
+# for attr in trainData.columns:
+# 	print(len(trainData[[attr]]))
+
+# for attr in embarkedOneHot.columns:
+# 	trainData[[attr]] = embarkedOneHot[[attr]]
+# for attr in trainOld.columns:
+# 	trainData[[attr]] = trainOld[[attr]]
+# # trainData = trainData.dropna()
+
+# # for attr in trainData.columns:
+# # 	print(len(trainData[[attr]]))
+# # trainData = trainData.add(embarkedOneHot[['Queenstown']], axis = 'columns')
+# # print(trainData.head(40))
+# # print(trainOld.head(40))
+# print(len(trainData[['Male']]), len(trainData[['Queenstown']]))
+# # trainData = pd.concat(trainData, embarkedOneHot, axis = 1)
+# # print(trainData.head(30))
+# # trainData = trainData.drop(['Embarked'])
